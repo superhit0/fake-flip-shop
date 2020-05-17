cb1e863d123ad3bfed1a3d4f8bd07c51f18df3e4
Abhimanyu Singh <abhimasingh@paypal.com>
2020-05-17 14:22:46 +0530

    Added redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import Reducers from './reducers';
+
+const store = createStore(Reducers);
 
 ReactDom.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
