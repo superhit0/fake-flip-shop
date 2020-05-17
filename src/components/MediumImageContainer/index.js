a62822faeb8791ffb8d0309811c810b5ec1232c6
Abhimanyu Singh <abhimasingh@paypal.com>
2020-05-17 19:02:45 +0530

    Added OverLay

--- src/components/MediumImageContainer/index.js
+++ src/components/MediumImageContainer/index.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import './styles.css';
 
 const mapStateToProps = ({ selectedImage }) => ({ selectedImage });
 
-const App = ({ selectedImage }) => {
-  return (
-    <div className="med-img-container">
-      <img className="med-img" src={`../../assets/med-img-${selectedImage}.jpeg`} />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOverLayHidden: true,
+      overLayPosition: {
+        left: '50%',
+        top: '50%'
+      }
+    };
+  }
+
+  showOverlay = (e) => {
+    console.log(e.clientX, e.clientY, e.currentTarget.getBoundingClientRect());
+    this.setState({
+      isOverLayHidden: false
+    })
+  }
+
+  hideOverlay = () => {
+    this.setState({
+      isOverLayHidden: true
+    })
+  }
+
+  render() {
+    const { isOverLayHidden, overLayPosition } = this.state;
+    const { selectedImage } = this.props;
+    const overlayClass = isOverLayHidden ? 'med-overlay hidden' : 'med-overlay';
+    return (
+      <div className="med-img-container" onMouseOver={this.showOverlay} onMouseOut={this.hideOverlay}>
+        <img className="med-img" src={`../../assets/med-img-${selectedImage}.jpeg`} />
+        <div className={overlayClass} style={overLayPosition}></div>
+      </div>
+    );
+  }
 };
 
 export default connect(mapStateToProps)(App);
